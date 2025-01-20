66a6c28e81fe9b95e5719139398ca7b3fb46ffd9
[JUNG RYUN KIM] <[kim665700@gmail.com]>
2025-01-20 09:44:23 +0900

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <main>라우팅</main>
+    </div>
+  )
+}
+
+export default App
