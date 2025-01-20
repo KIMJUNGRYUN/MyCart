738fe48168a6d9d4349cc06471c8dbde1a52ed16
[JUNG RYUN KIM] <[kim665700@gmail.com]>
2025-01-20 10:24:59 +0900

    linkitem

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,11 +6,11 @@ import idButton from '../../assets/id-button.png';
 import memo from '../../assets/memo.png';
 import order from '../../assets/package.png';
 import lock from '../../assets/locked.png';
+import LinkWithIcon from './LinkWithIcon';
 
 const Navbar = () => {
   return (
     <nav className='align_center navbar'>
-
     <div className='align_center'>
         <h1 className='navbar_heading'>myCart</h1>
         <form className='align_center navbar_form'>
@@ -20,13 +20,20 @@ const Navbar = () => {
             </button>
         </form>
     </div>
+    
     <div className='align_center navbar_links'>
-				<a href='#' className='align_center'>
-					Home <img src={rocket} alt='' className='link_emoji' />
+        <LinkWithIcon title='홈페이지' link='/' emoji={rocket} />
+        <LinkWithIcon title='상품들' link='/products' emoji={star} />
+        <LinkWithIcon title='로그인' link='/login' emoji={idButton} />
+        <LinkWithIcon title='회원가입' link='/signup' emoji={memo} />
+        <LinkWithIcon title='내 주문' link='/myorders' emoji={order} />
+        <LinkWithIcon title='로그아웃' link='/logout' emoji={lock} />
+      <a href='/cart' className='align_center'>
+					장바구니 <p className='align_center cart_counts'>0</p>
 				</a>
 			</div>
     <div className='align_center navbar_links'>
-        
+  
     </div>
 </nav>
   );
