710eb7858bd5dffd5c6631cdac8bf3bcbf882c05
[JUNG RYUN KIM] <[kim665700@gmail.com]>
2025-01-20 10:34:52 +0900

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
+import HomePage from './components/Home/HomePage'
 import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
-      <main>라우팅</main>
+      <main>
+        <HomePage />
+      </main>
     </div>
   )
 }
