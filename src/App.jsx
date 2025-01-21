391095f8c3382acf318fcb5d614e9d576c9d5b30
[JUNG RYUN KIM] <[kim665700@gmail.com]>
2025-01-21 11:05:49 +0900

    register form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import LoginPage from './components/Authentication/LoginPage'
+import SignupPage from './components/Authentication/SignupPage'
 import CartPage from './components/Cart/CartPage'
 
 import HomePage from './components/Home/HomePage'
@@ -20,7 +21,8 @@ const App = () => {
         {/* <SingleProductPage /> */}
         {/* <CartPage /> */}
         {/* <MyOrderPage /> */}
-        <LoginPage />
+        {/* <LoginPage /> */}
+        <SignupPage />
       </main>
     </div>
   );
