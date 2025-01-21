39b4a908170217163c0dd72b5dd49e66311ae984
[JUNG RYUN KIM] <[kim665700@gmail.com]>
2025-01-21 10:03:56 +0900

    react hook form

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -1,36 +1,46 @@
 import React, { useRef, useState } from 'react'
 import './LoginPage.css';
+import { useForm } from 'react-hook-form';
 
 const LoginPage = () => {
 	// useRef Hook 함수 
 	// const passwordRef = useRef(null);
+	
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	  } = useForm();
 
-	const [user, setUser] = useState({
-		email: '',
-		password: '',
-	});
+	const submitData = (formData) => console.log(formData);
+	  
+	
+	// const [user, setUser] = useState({
+	// 	email: '',
+	// 	password: '',
+	// });
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		console.log(user);
-		setUser({email:'', password:''}); 
-		// 초기값으로 바꿔주기
-	};
+	// const handleSubmit = (e) => {
+	// 	e.preventDefault();
+	// 	console.log(user);
+	// 	setUser({email:'', password:''}); 
+	// 	// 초기값으로 바꿔주기
+	// };
 
+	
 
 
   return (
     <section className='align_center form_page'>
-			<form onSubmit={handleSubmit} className='authentication_form'>
+			<form onSubmit={handleSubmit(submitData)} className='authentication_form'>
 				<h2>로그인 폼</h2>
 				<div className='form_inputs'>
 					<div>
 						<label htmlFor='email'>Email</label>
 						<input
-							onChange={(e) => setUser({...user, email: e.target.value})}
-							value={user.email}
-							type='email'
-							id='email'
+							{...register('email',{required: '이메일을 입력해주세요. '})}
+			
+							// onChange value 둘 다 해결
 							className='form_text_input'
 							placeholder='이메일 입력...'
 						/>
@@ -40,10 +50,7 @@ const LoginPage = () => {
 						<label htmlFor='password'>Password</label>
 						<input
 							// ref={passwordRef}
-							onChange={(e) => setUser({...user, password: e.target.value})}
-							value={user.password}
-							type='password'
-							id='password'
+							{...register('password')}
 							className='form_text_input'
 							placeholder='패스워드'
 						/>	
