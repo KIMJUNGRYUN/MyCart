c4d4fa89942be246b64133ba7964d32b9740e16e
[JUNG RYUN KIM] <[kim665700@gmail.com]>
2025-01-31 09:31:17 +0900

    FeaturedProducts

--- src/components/Home/FeaturedProducts.jsx
+++ src/components/Home/FeaturedProducts.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import './FeaturedProducts.css'
 import ProductCard from '../Products/ProductCard';
+import useData from '../../Hook/useData';
+import ProductCardSkeleton from '../Products/ProductCardSkeleton';
 
 const FeaturedProducts = () => {
+  const { data: products, error, isLoading } = useData("products/featured");
+	const skeletons = [1, 2, 3];
   return (
     <section className='featured_products'>
         <h2>주요제품</h2>
 
         <div className='align_center featured_products_list'>
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-             
+        {error && <em className='form_error'>{error}</em>}
+        {isLoading && skeletons.map((n) => <ProductCardSkeleton key={n} />)}
+				
+				{products && !isLoading &&
+					products.map((product) => (
+					<ProductCard
+						key={product._id}
+						product={product}
+					/>
+				))}
+
+				
+            
         </div>
     </section>
   );
